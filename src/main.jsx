58518e9bf9a300d7824e9d792d4002bfce031888
Fabián Forero <fabianforero2002@gmail.com>
2025-07-03 19:29:29 -0500

    Fix limpieza de dependencias y lockfile

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/700.css";
+import "./index.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
